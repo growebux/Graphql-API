050f84fb48d61081133d1248ced7905ed902c36d
Rafabc18 <70495348+growebux@users.noreply.github.com>
2022-05-15 16:55:47 -0700

    Add Mongoose and create AddDirector Mutation

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./Schema/schema");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config({ path: "./config.env" });
 
 const app = express();
 
-CONNECTION_URL =
-  "mongodb+srv://RafaelTraining:981506478@cluster0.yujwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(CONNECTION_URL);
-mongoose.connection.once("open", () => {
-  console.log("Connection Successful");
-});
+// CONNECTION_URL =
+//   "mongodb+srv://RafaelTraining:981506478@cluster0.yujwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// mongoose.connect(CONNECTION_URL);
+// mongoose.connection.once("open", () => {
+//   console.log("Connection Successful");
+// });
+const DB = process.env.DATABASE;
+mongoose.connect(DB, { useNewUrlParser: true });
 
 // Middleware - Interact with Graphql
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
